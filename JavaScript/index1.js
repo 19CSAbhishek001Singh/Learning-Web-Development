380a79cfa1332074c3ef4332c626cd54eff134ed
Abhishek <abhisheksinghcsanand@gmail.com>
2021-07-15 11:18:14 +0530

    learning basics

--- JavaScript/index1.js
+++ JavaScript/index1.js
@@ -1,4 +1,22 @@
-alert("hello world!");
-let x = "Hello ";
-let y = "Abhishek";
-console.log(x + y);
+// let x = "Hello ";
+// let y = "Abhishek";
+// console.log(x + y);
+
+// nullish coalescing operator??
+// let user;
+ // user="abhishek";
+// alert(user ?? "Guest User");
+
+// switch
+
+// loops
+// while,do while,for 
+
+// Typeconversion-auto and functions
+
+// string manipulation
+
+// Array
+
+// Operations on Array
+
