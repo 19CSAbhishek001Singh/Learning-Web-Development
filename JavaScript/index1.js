549a200fd7c1fd7805a405fcef210a26b1f07740
Abhishek <abhisheksinghcsanand@gmail.com>
2021-07-21 12:47:20 +0530

    javascript continued

--- JavaScript/index1.js
+++ JavaScript/index1.js
@@ -53,3 +53,33 @@
 // Object Constructor Function
 
 // Object Prototype
+
+// Hoisting
+
+// Strict mode
+
+// DOM - Document Object Model
+
+// Select an element by "id"
+
+// Select element by class name
+
+// Select element by Tag name
+
+// Query Selector
+
+// Traversing Elements
+
+// innerHtml
+
+// Create and append element
+
+// Insert before an element
+
+// Remove child element
+
+// Clone Element
+
+// Replace Element
+
+// Insert adjacent HTML
